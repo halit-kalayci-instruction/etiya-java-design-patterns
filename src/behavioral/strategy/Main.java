8cedce850b0778800eac8f8fbe4e9690dbceff4e
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-01-31 14:35:03 +0300

    strategy pattern

---
+++ src/behavioral/strategy/Main.java
@@ -0,0 +1,12 @@
+package behavioral.strategy;
+
+public class Main {
+    public static void main(String[] args) {
+        Context context = new Context(new OperationAdd());
+        System.out.println(context.executeStrategy(5,10));
+
+        context = new Context(new OperationMultiply());
+        System.out.println(context.executeStrategy(5,10));
+        
+    }
+}
